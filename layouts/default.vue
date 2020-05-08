7b279d014bfd428db23efc9c0491b2ac0a0acfec
Muradkhan0161 <murad.khan@northsouth.edu>
2020-05-08 23:20:11 +0600

    dashboard

---
+++ layouts/default.vue
@@ -0,0 +1,102 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      fixed
+    >
+      <v-list-item style="margin-top:5em">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Murad Khan</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Dashboard',
+          to: '/'
+        },
+        {
+          icon: 'mdi-apps',
+          title: 'pages',
+          to: '/pages'
+        },
+        {
+          icon: 'mdi-apps',
+          title: 'User control',
+          to: '/pages'
+        },
+        {
+          icon: 'mdi-apps',
+          title: 'Selling info',
+          to: '/pages'
+        },
+        {
+          icon: 'mdi-apps',
+          title: 'Products',
+          to: '/pages'
+        },
+        {
+          icon: 'mdi-apps',
+          title: 'google analytics',
+          to: '/pages'
+        },
+      ],
+    }
+  }
+}
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
